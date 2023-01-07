86e4a93154d8c4fc2222359fd361a13ea0016153
DESKTOP-QN8CGHH\willy <willy4543@gmail.com>
2023-01-07 23:21:48 +0800

    16

---
+++ leetcode/src/main/java/tw/william/leetcode/interview/Int0Xfff.java
@@ -0,0 +1,14 @@
+package tw.william.leetcode.interview;
+
+public class Int0Xfff {
+
+	public static void main(String[] args) {
+
+		
+		Integer a = 0xfff;
+		
+		System.out.println(a);
+		
+	}
+
+}
